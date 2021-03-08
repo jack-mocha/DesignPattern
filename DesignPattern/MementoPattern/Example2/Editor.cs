853d2aecbce588eec5a48b8897e5dbaaccecd6ee
Jack Hsu <jackhsu0225@gmail.com>
2021-03-08 16:47:02 -0500

    Add example 2 that uses nested class

---
+++ DesignPattern/MementoPattern/Example2/Editor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.MementoPattern.Example2
+{
+    //This example utilizes nested class.
+    public class Editor
+    {
+        public string Content { get; set; }
+
+        public EditorState CreateState()
+        {
+            return new EditorState(this.Content);
+        }
+
+        public void RestoreState(EditorState state)
+        {
+            this.Content = state.Content;
+        }
+
+        public class EditorState
+        {
+            public string Content { get; private set; }
+
+            public EditorState(string content)
+            {
+                this.Content = content;
+            }
+        }
+    }
+}
---
+++ DesignPattern/MementoPattern/Example2/History.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static DesignPattern.MementoPattern.Example2.Editor;
+
+namespace DesignPattern.MementoPattern.Example2
+{
+    public class History
+    {
+        private Stack<EditorState> stk = new Stack<EditorState>();
+
+        public void Push(EditorState state)
+        {
+            stk.Push(state);
+        }
+
+        public EditorState Pop()
+        {
+            return stk.Count != 0 ? stk.Pop() : null;
+        }
+    }
+}
